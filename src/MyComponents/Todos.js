d6f78a3a2058711a122c1b252f9cdfdd9c7b2584
rahul reddy <rahulreddy.adasada@gmail.com>
2021-08-26 12:51:26 +0530

    todolist with react.js

---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { TodoItem } from "./TodoItem";
+
+export const Todos = (props) => {
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "40px auto",
+  };
+
+  return (
+    <div className="container my-3" style={myStyle}>
+      <h3 className="my-3">Todos list</h3>
+      {props.todos.length === 0
+        ? "no todos to display"
+        : props.todos.map((todo) => {
+            return <TodoItem todo={todo} onDelete={props.onDelete} />;
+          })}
+    </div>
+  );
+};
